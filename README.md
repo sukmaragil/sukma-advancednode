4ddd86d43d70b78ef7059ed0d195e6a1e178bea6
sukmaragil <amaliya.pristiyanto@shopee.com>
2023-12-22 15:45:48 +0700

    try sync with repl.it

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Advanced Node and Express
+
+This is the boilerplate for the Advanced Node and Express lessons. Instructions for the lessons start at https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/
 # sukma-advancednode
---
+++ auth.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
+const { ObjectID } = require("mongodb");
+const githubStrategy = require("passport-github").Strategy;
+
+module.exports = function (app, myDatabase) {
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    }),
+  );
+
+  passport.use(
+    new GithubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "https://sukma-advancednode.sukmaragil.repl.co/",
+      },
+      function (accessToken, refresToken, profile, cb) {
+        console.log(profile);
+        //Database logic here with callback containing your user object
+        myDatabase.findOneAndUpdate(
+          {
+            id: profile.id,
+          },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              username: profile.username,
+              name: profile.displayName || "John Doe",
+              photo: profile.photos[0].value || "",
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : "No public email",
+              provider: profile.provider || "",
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          },
+        );
+      },
+    ),
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+};
